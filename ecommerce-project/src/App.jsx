807d9b814ab6298e349945ed318623878e396cc3
danielistifanusbello@gmail.com <distifan@sfu.ca>
2025-06-05 01:59:23 -0700

    Create Pages using routers

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,8 +1,14 @@
 import { HomePage } from "./pages/HomePage";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<div>checkout page</div>} />
+    </Routes>
+  );
 }
 
 export default App;
